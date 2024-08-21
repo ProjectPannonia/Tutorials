1e23ef7ef098e4815003dd16399b8ba5636c1f87
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2024-08-21 20:25:25 +0200

    Add project files.

---
+++ Tutorials/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tutorials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main thread";
+            //Console.WriteLine(mainThread.Name);
+
+            // Thread - passed methods without parameters
+            //Thread thread1 = new Thread(CountDown);
+            //Thread thread2 = new Thread(CountUp);
+
+            Thread thread1 = new Thread(() => CountDown("Timer #1"));
+            Thread thread2 = new Thread(() => CountUp("Timer #2"));
+            thread1.Start();
+            thread2.Start();
+
+            Console.WriteLine(mainThread.Name + " is complete!");
+
+            Console.ReadKey();
+        }
+
+        public static void CountDown(string name)
+        {
+            for (int i = 10; i > 0; i--) 
+            {
+                Console.WriteLine("Timer #1: " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #1 is complete!");
+        }
+        public static void CountUp(string name)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Timer #2: " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #2 is complete!");
+        }
+    }
+}
